ac19ec726ecb8491553d7ce036e22314a2a364b9
Shivam <shivamsinghl498@gmail.com>
2021-08-31 02:27:36 +0530

    corousel

--- p2/src/Heading.jsx
+++
@@ -1,6 +0,0 @@
-import './style.css';
-export default function Heading(props){
-    return (
-        <h2 className="head">{props.head}</h2>
-    );
-};
---
+++ p2/src/Helpers/CarouselData.js
@@ -0,0 +1,29 @@
+import City from '../images/america.jpg';
+import City1 from '../images/japan.jpg';
+import City2 from '../images/india.jpg';
+import City3 from '../images/canada.jpg';
+
+
+
+export const images=[
+    {
+        title: "New York",
+        subtitle: "come to new york",
+        img: City
+    },
+    {
+        title: "japan",
+        subtitle: "chao chao",
+        img: City1
+    },
+    {
+        title: "India",
+        subtitle: "namaskar",
+        img: City2
+    },
+    {
+        title: "Vancouver",
+        subtitle: "coolam coolam",
+        img: City3
+    }
+];
